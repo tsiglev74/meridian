a222b0673612c445af1d9d0c951a1def3aad1a58
Lukasz Mazurek <lukmaz@google.com>
2025-07-22 14:05:37 -0700

    Add unit test for adstock weights computation.
    PiperOrigin-RevId: 785989193

--- meridian/model/adstock_hill_test.py
+++ meridian/model/adstock_hill_test.py
@@ -94,7 +94,7 @@ class TestAdstock(parameterized.TestCase):
       ),
       dict(
           testcase_name="max_lag > n_media_times",
-          media=_MEDIA[..., :(_MAX_LAG - 1)],
+          media=_MEDIA[..., : (_MAX_LAG - 1)],
           alpha=_ALPHA,
           n_time_output=_N_MEDIA_TIMES,
       ),
@@ -109,7 +109,7 @@ class TestAdstock(parameterized.TestCase):
     """Basic test for valid output."""
     media_transformed = adstock_hill.AdstockTransformer(
         alpha, self._MAX_LAG, n_time_output
-        ).forward(media)
+    ).forward(media)
     output_shape = tf.TensorShape(
         alpha.shape[:-1] + media.shape[-3] + [n_time_output] + alpha.shape[-1]
     )
@@ -169,8 +169,7 @@ class TestAdstock(parameterized.TestCase):
     # n_nonzero_terms is a tensor with length containing the number of nonzero
     # terms in the adstock for each output time period.
     n_nonzero_terms = np.minimum(
-        np.arange(1, self._N_MEDIA_TIMES + 1),
-        self._MAX_LAG + 1
+        np.arange(1, self._N_MEDIA_TIMES + 1), self._MAX_LAG + 1
     )
     # For each output time period and alpha value, the adstock is given by
     # adstock = series1 / series2, where:
@@ -190,10 +189,28 @@ class TestAdstock(parameterized.TestCase):
     result = term1 / term2[:, :, None, :]
     # Broadcast `result` across geos.
     result = tf.tile(
-        result[:, :, None, :, :],
-        multiples=[1, 1, self._N_GEOS, 1, 1]
+        result[:, :, None, :, :], multiples=[1, 1, self._N_GEOS, 1, 1]
     )
     tf.debugging.assert_near(media_transformed, result)
+
+  def test_output(self):
+    """Test for valid adstock weights."""
+    alpha = tf.constant([0.1, 0.5, 0.9])
+    window_size = 5
+    media = tf.constant([[
+        [0.12, 0.55, 0.89],
+        [0.34, 0.71, 0.23],
+        [0.91, 0.08, 0.67],
+        [0.45, 0.82, 0.11],
+        [0.78, 0.29, 0.95],
+    ]])
+    adstock = adstock_hill.AdstockTransformer(
+        alpha=alpha,
+        max_lag=window_size - 1,
+        n_times_output=1,
+    ).forward(media)
+    expected_adstock = tf.constant([0.751, 0.435, 0.572])
+    tf.debugging.assert_near(adstock, expected_adstock, rtol=1e-2)
 
 
 class TestHill(parameterized.TestCase):
