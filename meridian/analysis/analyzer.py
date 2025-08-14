Fix scaling non_media_treatments in incremental_outcome
PiperOrigin-RevId: 795203205

meridian/analysis/analyzer.py
@@ -2035,7 +2035,7 @@ class Analyzer:
           tf.constant(
               non_media_treatments_baseline_normalized, dtype=tf.float32
           )[tf.newaxis, tf.newaxis, :],
-          self._meridian.non_media_treatments.shape,  # pytype: disable=attribute-error
+          data_tensors.non_media_treatments.shape,  # pytype: disable=attribute-error
       )
     else:
       non_media_treatments_baseline_normalized = None